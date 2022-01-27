418035f1768f29c603c03146c354b658a2b25443
Loc Lam <lpl44@drexel.edu>
2022-01-26 23:50:32 -0500

    updated main.c

--- main.c
+++ main.c
@@ -1,26 +1,13 @@
+#include <sys/stat.h>
+
+#include <ctype.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/*
- * pl0c -- PL/0 compiler.
- *
- * program	= block "." .
- * block	= [ "const" ident "=" number { "," ident "=" number } ";" ]
- *		  [ "var" ident { "," ident } ";" ]
- *		  { "procedure" ident ";" block ";" } statement .
- * statement	= [ ident ":=" expression
- *		  | "call" ident
- *		  | "begin" statement { ";" statement } "end"
- *		  | "if" condition "then" statement
- *		  | "while" condition "do" statement ] .
- * condition	= "odd" expression
- *		| expression ( "=" | "#" | "<" | ">" ) expression .
- * expression	= [ "+" | "-" ] term { ( "+" | "-" ) term } .
- * term		= factor { ( "*" | "/" ) factor } .
- * factor	= ident
- *		| number
- *		| "(" expression ")" .
- */
+#include <string.h>
+#include <unistd.h>
 
 #define TOK_IDENT	'I'
 #define TOK_NUMBER	'N'
@@ -50,40 +37,41 @@
 #define TOK_LPAREN	'('
 #define TOK_RPAREN	')'
 
-static void error(const char *fmt, ...);
-static void readin(char *file);
+/*
+ * pl0c -- PL/0 compiler.
+ *
+ * program	= block "." .
+ * block	= [ "const" ident "=" number { "," ident "=" number } ";" ]
+ *		  [ "var" ident { "," ident } ";" ]
+ *		  { "procedure" ident ";" block ";" } statement .
+ * statement	= [ ident ":=" expression
+ *		  | "call" ident
+ *		  | "begin" statement { ";" statement } "end"
+ *		  | "if" condition "then" statement
+ *		  | "while" condition "do" statement ] .
+ * condition	= "odd" expression
+ *		| expression ( "=" | "#" | "<" | ">" ) expression .
+ * expression	= [ "+" | "-" ] term { ( "+" | "-" ) term } .
+ * term		= factor { ( "*" | "/" ) factor } .
+ * factor	= ident
+ *		| number
+ *		| "(" expression ")" .
+ */
 
-char *raw;
-
-/* Main. */
-int
-main(int argc, char *argv[])
-{
-	char *startp;
-
-	if (argc != 2) {
-		(void) fputs("usage: pl0c file.pl0\n", stderr);
-		exit(1);
-	}
-
-	readin(argv[1]);
-	startp = raw;
-
-	parse();
-
-	free(startp);
-
-	return 0;
-}
-
+static char *raw, *token;
+static int type;
 static size_t line = 1;
 
-/* Misc. functions. */
+/*
+ * Misc. functions.
+ */
+
 static void
 error(const char *fmt, ...)
 {
 	va_list ap;
-	(void) fprintf(stderr, "p10c: error: %lu: ", line);
+
+	(void) fprintf(stderr, "pl0c: error: %lu: ", line);
 
 	va_start(ap, fmt);
 	(void) vfprintf(stderr, fmt, ap);
@@ -99,11 +87,12 @@ readin(char *file)
 {
 	int fd;
 	struct stat st;
-	if (strrchr(file, '.') == NULL)
-		error("file must end in '.p10'");
 
-	if (!!strcmp(strrchr(file, '.'), ".p10"))
-		error("file must end in '.p10'");
+	if (strrchr(file, '.') == NULL)
+		error("file must end in '.pl0'");
+
+	if (!!strcmp(strrchr(file, '.'), ".pl0"))
+		error("file must end in '.pl0'");
 
 	if ((fd = open(file, O_RDONLY)) == -1)
 		error("couldn't open %s", file);
@@ -112,7 +101,7 @@ readin(char *file)
 		error("couldn't get file size");
 
 	if ((raw = malloc(st.st_size + 1)) == NULL)
-		error("malloc failed")
+		error("malloc failed");
 
 	if (read(fd, raw, st.st_size) != st.st_size)
 		error("couldn't read %s", file);
@@ -120,10 +109,6 @@ readin(char *file)
 
 	(void) close(fd);
 }
-
-static char *raw, *token;
-static int type;
-static size_t line = 1;
 
 /*
  * Lexer.
@@ -322,4 +307,28 @@ parse(void)
 		}
 		(void) fputc('\n', stdout);
 	}
+}
+
+/*
+ * Main.
+ */
+
+int
+main(int argc, char *argv[])
+{
+	char *startp;
+
+	if (argc != 2) {
+		(void) fputs("usage: pl0c file.pl0\n", stderr);
+		exit(1);
+	}
+
+	readin(argv[1]);
+	startp = raw;
+
+	parse();
+
+	free(startp);
+
+	return 0;
 }
